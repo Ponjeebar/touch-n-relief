54d174b8dd5fe615cf6cd708dc883563317e429d
Resty <Resty@gmail.com>
2026-09-23 19:10:48 +0800

    Hide chatbot on mobile booking page

--- resources/views/booking/create.blade.php
+++ resources/views/booking/create.blade.php
@@ -14,7 +14,7 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
     @include('partials.chatbot-assets')
 </head>
-<body>
+<body class="booking-page-body">
     @include('partials.landing-nav', ['navMode' => 'booking', 'solidNav' => true])
 
     <main class="booking-page">
